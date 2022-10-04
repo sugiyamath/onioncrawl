7f43a7b3ac8ddeee8c036ecd8cd66eb8612d8e10
Luna Halfmoon <mathgeekjp@gmail.com>
2022-10-04 23:32:11 +0900

    update

--- crawl.py
+++ crawl.py
@@ -12,27 +12,47 @@ def crawl():
     URLS = set(_load_initials())
     while URLS:
         url = URLS.pop()
-        if ".onion" not in url:
+        if not _check_url(url):
             continue
-        if url in ARRIVED:
+        success, html, rurl = _download(url)
+        if not success:
             continue
-        try:
-            r = requests.get(url, timeout=10)
-            html = r.content.decode("utf-8")
-            _save(html, url)
-        except:
-            ARRIVED.add(url)
-            continue
-        for x in re.findall(reg, html):
-            if x.startswith("http"):
-                URLS.add(x)
-            else:
-                if x.startswith("/") and r.url.endswith("/"):
-                    x = x[1:]
-                elif not x.startswith("/") and not r.url.endswith("/"):
-                    x = "/" + x                
-                URLS.add(r.url+x)
+        _extract_urls(html, rurl, reg)
+
+
+def _extract_urls(html, rurl, reg):
+    for x in re.findall(reg, html):
+        if x.startswith("http"):
+            URLS.add(x)
+        else:
+            if x.startswith("/") and rurl.endswith("/"):
+                x = x[1:]
+            elif not x.startswith("/") and not rurl.endswith("/"):
+                x = "/" + x                
+            URLS.add(rurl+x)
+
+
+def _download(url):
+    try:
+        r = requests.get(url, timeout=10)
+        html = r.content.decode("utf-8")
+        _save(html, url)
+        flag = True
+        rurl = r.url
+    except:
+        flag = False
+        html = None
+        rurl = None
+    finally:
         ARRIVED.add(url)
+    return flag, html, rurl
+        
+def _check_url(url):
+    if ".onion" not in url:
+        return False
+    if url in ARRIVED:
+        return False
+    return True
 
 
 def _load_initials(path="initials.txt"):
